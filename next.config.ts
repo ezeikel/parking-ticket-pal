6ed191bda59e2f39e513b5156c8c9c621c344237
Ezeikel Pemberton <hi@ezeikel.dev>
2025-08-08 19:11:56 +0100

    fix: wildcard for blb storage urls

--- next.config.ts
+++ next.config.ts
@@ -41,7 +41,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: '4hdddimltnxummbb.public.blob.vercel-storage.com',
+        hostname: '**.public.blob.vercel-storage.com',
         pathname: '**',
       },
     ],
